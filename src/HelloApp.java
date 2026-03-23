641696bb2f51dbb85efe6cdb0b43bb97ab3dd4d2
Shanmukhjsd <shanmukamolabanti2008@gmail.com>
2026-03-23 16:52:48 +0530

    UC6: Join names and remove trailing comma using substring

--- src/HelloApp.java
+++ src/HelloApp.java
@@ -1,7 +1,16 @@
 public class HelloApp {
     public static void main(String[] args) {
         if (args.length > 0) {
-            System.out.println("Hello " + args[0]);
+            String result = "";
+
+            for (String name : args) {
+                result += name + ", ";
+            }
+
+            // Remove last comma and space
+            result = result.substring(0, result.length() - 2);
+
+            System.out.println("Hello " + result);
         } else {
             System.out.println("Hello User");
         }
